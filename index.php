19c7cbe89cd6fd7f516d89b5a4e567b6eed01c8f
Jacques Lucke <Jacques@moonicfx.com>
2014-05-01 12:49:56 +0200

    add main website structure

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <html>
 	<head>
+		<title>Mathehilfe</title>
 	</head>
 	<body>
+		<div id="website">
+			<div id="header">
+			</div>
+			<div id="status">
+			</div>
+			<div id="currentTask">
+			</div>
+			<div id="footer">
+			</div>
+		</div>
 	</body>
 </html>
