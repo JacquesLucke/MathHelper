e99395f9419709c52f57d55cf31b15dedcc28309
Jacques Lucke <Jacques@moonicfx.com>
2014-05-01 18:47:19 +0200

    rename folders

--- kernel/taskManager.php
+++ kernel/taskManager.php
@@ -1,10 +1,9 @@
 <?php
-	include("TaskTypes/SimpleQuestionAnswerType.php");
+	include("types/SimpleQuestionAnswerType.php");
 	
 	function GetTaskFromString($taskString)
 	{
 		list($fileName, $taskName, $optional) = explode("-.-", $taskString);
-		echo "$fileName---$taskName---$optional";
 	
 		$t = new SimpleQuestionAnswerType("was ist 2+2?", "4");
 		return $t;
