9a9531877980b44caf4f42451c37de62cb9b1717
Jacques Lucke <Jacques@moonicfx.com>
2014-05-01 18:40:39 +0200

    start task extraction of the string

--- kernel/taskManager.php
+++ kernel/taskManager.php
@@ -3,6 +3,9 @@
 	
 	function GetTaskFromString($taskString)
 	{
+		list($fileName, $taskName, $optional) = explode("-.-", $taskString);
+		echo "$fileName---$taskName---$optional";
+	
 		$t = new SimpleQuestionAnswerType("was ist 2+2?", "4");
 		return $t;
 	}
