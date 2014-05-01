99a47171b41fb1d35e121e2530e0344b56be1720
Jacques Lucke <Jacques@moonicfx.com>
2014-05-01 13:51:53 +0200

    layout and a bit text

--- index.php
+++ index.php
@@ -7,16 +7,21 @@
 	<body>
 		<div id="website">
 			<div id="header">
+				<h1>Mathehilfe</h1>
 			</div>
 			<div id="status">
+				nothing done jet
 			</div>
 			<div id="currentTask">
 				<div id="actualTask">
+					please load a task
 				</div>
 				<div id="help">
+					Help for the task
 				</div>
 			</div>
 			<div id="footer">
+				<span>von Jacques Lucke</span>
 			</div>
 		</div>
 	</body>
