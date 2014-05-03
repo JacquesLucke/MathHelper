3623732403e630c81d5fa282eadec26f9ff50348
Jacques Lucke <Jacques@moonicfx.com>
2014-05-04 01:13:56 +0200

    better exception handling, when there is no task created

--- kernel/taskManager.php
+++ kernel/taskManager.php
@@ -4,11 +4,18 @@
 	
 	function GetTaskFromString($taskString)
 	{
+		// extract path and name from string
 		list($path, $taskName, $optional) = explode("-.-", $taskString);
 		$path = "tasks/". $path .".php";
+		
+		// execute the function, result should be a task
 		$e = (string)include($path);
-		if($e == "1" && function_exists($taskName)) return $taskName($optional);
-		else return new ErrorType;
+		if($e == "1" && function_exists($taskName)) $task = $taskName($optional);	
+		
+		//if there is no task coming back, an error task is created tp prevent later errors
+		if(!method_exists($task, "AddTask")) $task = new ErrorType();
+		
+		return $task;
 	}
 	
 	class ErrorType
