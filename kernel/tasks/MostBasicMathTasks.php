d81034a85a8af4e150b0cca52aee2c4934c4048f
Jacques Lucke <Jacques@moonicfx.com>
2014-05-03 19:41:26 +0200

    extend basic math tasks by subtract, multiply and divide

--- kernel/tasks/MostBasicMathTasks.php
+++ kernel/tasks/MostBasicMathTasks.php
@@ -1,10 +1,55 @@
 <?php
 	function GenerateBasicMathTask($data)
 	{
-		$z1 = rand(2, 100);
-		$z2 = rand(2, 100);
-		$t = new SimpleQuestionAnswerType($z1 ."+". $z2, $z1 + $z2);
-		return $t;
+		$random = rand(1, 4);
 		
+		// add
+		if($random == 1)
+		{
+			$z1 = rand(2, 100);
+			$z2 = rand(2, 100);
+			$t = new SimpleQuestionAnswerType($z1 ."+". $z2, $z1 + $z2);
+			return $t;
+		}
+		
+		// subtract
+		if($random == 2)
+		{
+			$z1 = rand(2, 100);
+			$z2 = rand(2, 100);
+			$t = new SimpleQuestionAnswerType($z1 ."-". $z2, $z1 - $z2);
+			return $t;
+		}
+		
+		// multiply
+		if($random == 3)
+		{
+			$type = rand(1, 3);
+			// same size
+			if($type <= 2)
+			{
+				$z1 = rand(2, 20);
+				$z2 = rand(2, 20);
+				$t = new SimpleQuestionAnswerType($z1 ."*". $z2, $z1 * $z2);
+				return $t;
+			}
+			// big difference
+			if($type == 3)
+			{
+				$z1 = rand(2, 10);
+				$z2 = rand(50, 1000);
+				$t = new SimpleQuestionAnswerType($z1 ."*". $z2, $z1 * $z2);
+				return $t;
+			}
+		}
+		
+		// divide
+		if($random == 4)
+		{
+			$z1 = rand(2, 50);
+			$z2 = $z1 * rand(2, 15);
+			$t = new SimpleQuestionAnswerType($z2 .":". $z1, $z2 / $z1);
+			return $t;
+		}
 	}
 ?>
