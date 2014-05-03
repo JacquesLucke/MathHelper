d8de3b45dae70938e1458f02c2b8d0fa1fd6d61d
Jacques Lucke <Jacques@moonicfx.com>
2014-05-03 23:25:15 +0200

    smallest change ever :o

--- kernel/tasks/MentalMathTasks.php
+++ kernel/tasks/MentalMathTasks.php
@@ -72,7 +72,7 @@
 		$t->help .= '<a href="?task=MentalMathTasks-.-GenerateMentalMathTask-.-3">Multiplizieren üben</a></br>';
 		$t->help .= '<a href="?task=MentalMathTasks-.-GenerateMentalMathTask-.-4">Dividieren üben</a></br>';
 		$t->help .= '<a href="?task=MentalMathTasks-.-GenerateMentalMathTask-.-5">Wurzel ziehen üben</a></br>';
-		$t->help .= '<a href="?task=MentalMathTasks-.-GenerateMentalMathTask-.-6"Quadrieren üben</a></br>';
+		$t->help .= '<a href="?task=MentalMathTasks-.-GenerateMentalMathTask-.-6">Quadrieren üben</a></br>';
 		
 		return $t;
 	}
