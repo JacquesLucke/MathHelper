24eb0fe030996b5b26478ae5c924744ff354664c
Jacques Lucke <Jacques@moonicfx.com>
2014-05-09 23:44:44 +0200

    first implementation of MultipleChoiceType

---
+++ kernel/types/MultipleChoiceType.php
@@ -0,0 +1,34 @@
+<?php
+	class SimpleQuestionAnswerType
+	{
+	
+		public function __construct()
+		{
+		}
+	
+		public function SetTaskTitle()
+		{
+			echo "<title>Train Yourself</title>";
+		}
+
+		public function AddTaskStyle()
+		{
+		}
+		
+		public function AddTask()
+		{
+		}
+		
+		public function AddScript()
+		{
+		}
+		
+		public function AddTaskLinks()
+		{
+		}
+		
+		public function AddHelp()
+		{
+		}
+	}
+?>
