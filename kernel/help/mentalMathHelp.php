d2a285e6102d94d899c0235747f6e5ea6ffc246b
Jacques Lucke <Jacques@moonicfx.com>
2014-05-10 12:57:30 +0200

    delete help file because it is unused

--- kernel/help/mentalMathHelp.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-	function MultiplyFractionsHelp()
-	{
-		?>
-			Um zwei Brüche zu Multiplizieren rechnet man Zähler mal Zähler und Nenner mal Nenner.
-			<div class="math">\frac{a}{c}*\frac{b}{d}=\frac{a*b}{c*d}</div>
-		<?php
-	}
-?>
