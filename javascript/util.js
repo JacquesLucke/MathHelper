8a3ffb84a3a26893fdf20e0e7189be2bc8f9cea6
Jacques Lucke <Jacques@moonicfx.com>
2014-05-13 15:25:53 +0200

    changes to test design

--- javascript/util.js
+++ javascript/util.js
@@ -1,4 +1,14 @@
 function CompareStrings(a, b)
 {
+	a = UnifyChars(a);
+	b = UnifyChars(b);
 	return a == b;
+}
+
+// replaces different chars for easier comparison later
+function UnifyChars(a)
+{
+	a = a.replace(/,/g, ".");
+	a = a.replace(/:/g, "/");
+	return a;
 }
